0d5d2290e8c3a9e6d219b90080522d491af22601
DeShaun Thomas <dthomas1618@gmail.com>
2023-06-20 08:48:09 -0400

    add splash screen, app icon and logo

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 2000);
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-[#FEDF5E]">
-      <Text className="text-black">Open up App.js to start working on your app!</Text>
+       <Text>Yellow Bird</Text>
       <StatusBar style="auto" />
     </View>
   );
